08eb0ca063ac83f710018293c3537d802b79782b
Nakana.Tra <nakana.traore@live.fr>
2023-01-24 11:04:23 +0100

    changes app

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Bonjour !!!!!</h1>
+      <h1>Bonjour vous tous !!!!!</h1>
       <h2>C'est bon je me suis connectée !!</h2>
     </div>
   );
